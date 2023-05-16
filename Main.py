1c0f3d4abbacdba84028bc7e1a17c2ae62ba517e
Faa238 <afaf23himmatul@gmail.com>
2023-05-16 21:55:29 +0700

    Add new math functions (multiplication and division)

--- Main.py
+++ Main.py
@@ -1,14 +1,20 @@
-from math_function import add
+from math_function import *
 
 
 def main():
 
-    data_1 = int(input("masukkan input 1 :"))
-    data_2 = int(input("masukkan input 2 :"))
-    operator = input("masukkan operator :")
+    data_1 = int(input("Masukkan input 1 :"))
+    data_2 = int(input("Masukkan input 2 :"))
+    operator = input("Masukkan operator :") 
 
     if operator == "+":
         result = add(data_1, data_2)
+    elif operator == "*":
+        result = add(data_1, data_2)
+    elif operator == "/":
+        result = add(data_1, data_2)
+    else:
+        print("Operator tidak tersedia, hanya operator penjumlahan (+), perkalianan (*), pembagian (/) yang tersedia")
 
     print("{} {} {} = {} ".format(data_1, operator, data_2, result))
 
